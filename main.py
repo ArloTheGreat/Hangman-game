c195d0247a8d7c80e112d043c66eaf1a59261fc5
Ben123Mason <160073273+Ben123Mason@users.noreply.github.com>
2024-06-18 15:07:29 +1200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ lives = 10
 #------------------FUNCTIONS-----------------
 
 import random
-words = ("print", "words", "lucky", "space", "enter", "happy", "slope", "" ) 
+words = ("print", "words", "lucky", "space", "enter", "happy", "slope", "speed","brain","alone","cakes","dance","extra","farms","gamer","igloo","jeans","knock","laugh","messy","noble","ocean", "paint","queen", "radio", "salad", "table", "ultra", "voice", "wacky", "young", "zebra" ) 
+print (random.choice(words))
 
 
 
