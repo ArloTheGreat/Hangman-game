da4c5d04ad4ccff4447eb9d0ec8bfbd4ea354983
ArloTheGreat <159732916+ArloTheGreat@users.noreply.github.com>
2024-06-18 15:04:17 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #--------------------VALUES-------------------
-
+lives = 10
 
 
 
@@ -14,3 +14,17 @@ words = ("print", "words", "lucky", "space", "enter", "happy", "slope", "" )
 #--------------------MAIN--------------------
 
 print("Hello Word")
+
+#Hangman step depending on the
+if lives == 10:
+    print("____________")
+    print("|      |")
+    print("|    ('-')")
+    print("|     /|\ ")
+    print("|    / | \  ")
+    print("|     / \  ")
+    print("|    /   \  ")
+    print("|             ")
+    print("|____________")
+elif lives == 9:
+    print("eggs")
