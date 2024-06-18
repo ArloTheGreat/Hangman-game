cc654b9edf716f2fa4864ac6e8f06aa421883f7f
Ben123Mason <160073273+Ben123Mason@users.noreply.github.com>
2024-06-18 15:06:14 +1200

    added words

--- main.py
+++ main.py
@@ -6,7 +6,8 @@
 
 #------------------FUNCTIONS-----------------
 import random
-words = ("print", "words", "lucky", "space", "enter", "happy", "slope", "speed" ) 
+words = ("print", "words", "lucky", "space", "enter", "happy", "slope", "speed","brain","alone","cakes","dance","extra","farms","gamer","igloo","jeans","knock","laugh","messy","noble","ocean", "paint","queen", "radio", "salad", "table", "ultra", "voice", "wacky", "young", "zebra" ) 
+print (random.choice(words))
 
 
 
