406ec04b491eb08c01cae52a94c83012107465ea
ryenn890 <160070960+ryenn890@users.noreply.github.com>
2024-06-20 10:03:45 +1200

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,14 @@
 #------------------FUNCTIONS-----------------
 
 
-def intro():
-    print("test")
 
+name = input("What your name?")
+print("Haiii,Walcome to the game",name)
+print("This game about hangman")
+print("Here is how youplay hangman?")
+print("The participants playing have 10 chances to guess a random 5 letter word.")
+print("Good job,That's the end")
 
 #--------------------MAIN--------------------
 
 
-intro()
