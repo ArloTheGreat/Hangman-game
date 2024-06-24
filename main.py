5fae6c52b2f0b3a4b9d2759f21cab89005c58773
bnuaWSCW <nuab@wsc.school.nz>
2024-06-24 20:50:36 +1200

    Brandon commit 4.0

--- main.py
+++ main.py
@@ -39,177 +39,158 @@ while answer == "yes":
 # - Lives - #
     lives = 10
 
-    print("Hello Word")
 
 
-    print("Hello Word")
 
     # -- Getting a random word from the 5 letter list
     differentword = random.choice(words)
 
-    lives = 10
+
 
 
     while lives > 0:
-
-
-
-        # ----- GAME PROCESS
         while True:
             length = guess = input("Please guess a letter:")
 
             length = len(guess)
 
-            if length > 1:
-                print("Please guess 1 letter at a time")
-            elif length == 0:
-                print("Please guess a letter")
+            try:
+                int(guess)
+                print("\nPlease type in a letter. There's no numbers in a word!")
+            except:
+                print("")
+            if guess.isalpha():
+                guessed_words.append(guess)
+                break
+            elif length > 1:
+                print("Please guess 1 letter at a time\n")
+            elif length == 0 or guess == " ":
+                print("You haven't guessed a letter\n")
+            elif guess in guessed_words:
+                print("You have already guessed this letter")
 
-while lives > 0:
-    while True:
-        length = guess = input("Please guess a letter:")
-
-        length = len(guess)
-
-        try:
-            int(guess)
-            print("\nPlease type in a letter. There's no numbers in a word!")
-        except:
-            print("\n")
-
-        if length > 1:
-            print("Please guess 1 letter at a time\n")
-        elif length == 0 or guess == " ":
-            print("You haven't guessed a letter\n")
-        elif guess in guessed_words:
-            print("You have already guessed this letter")
-
-        if length == 1 and guess != " " and guess not in guessed_words:
-            guessed_words.append(guess)
-            break
-    
-    print(guessed_words)
+        print(guessed_words)
 
 
 
 
 #Hangman step depending on the
-if lives == 10:
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-elif lives == 9:
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ")
-    print(" ____________")
-elif lives == 8:
-    print(" ")
-    print("|    ")
-    print("|  ")
-    print("|     ")
-    print("|    ")
-    print("|   ")
-    print("|")
-    print("|             ")
-    print("|____________")
-elif lives == 7:
-    print("____________")
-    print("|      ")
-    print("|    ")
-    print("|      ")
-    print("|     ")
-    print("|       ")
-    print("|    ")
-    print("|             ")
-    print("|____________")
-elif lives == 6:
-    print("____________")
-    print("|      |")
-    print("|    ")
-    print("|      ")
-    print("|      ")
-    print("|      ")
-    print("|     ")
-    print("|             ")
-    print("|____________")
-elif lives == 5:
-    print("____________")
-    print("|      |")
-    print("|    ('-')")
-    print("|     ")
-    print("|     ")
-    print("|      ")
-    print("|     ")
-    print("|             ")
-    print("|____________")
-elif lives == 4:
-    print("____________")
-    print("|      |")
-    print("|    ('-')")
-    print("|      |")
-    print("|      |   ")
-    print("|      ")
-    print("|     ")
-    print("|             ")
-    print("|____________")
-elif lives == 3:
-    print("____________")
-    print("|      |")
-    print("|    ('-')")
-    print("|     /| ")
-    print("|    / |   ")
-    print("|        ")
-    print("|         ")
-    print("|             ")
-    print("|____________")
-elif lives == 2:
-    print("____________")
-    print("|      |")
-    print("|    ('-')")
-    print("|     /|\ ")
-    print("|    / | \  ")
-    print("|        ")
-    print("|        ")
-    print("|             ")
-    print("|____________")
-elif lives == 1:
-    print("____________")
-    print("|      |")
-    print("|    ('-')")
-    print("|     /|\ ")
-    print("|    / | \  ")
-    print("|     /   ")
-    print("|    /    ")
-    print("|             ")
-    print("|____________")
-elif lives == 0:
-    print("____________")
-    print("|      |")
-    print("|    (X-X)")
-    print("|     /|\ ")
-    print("|    / | \  ")
-    print("|     / \  ")
-    print("|    /   \  ")
-    print("|             ")
-    print("|____________")
+    if lives == 10:
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+    elif lives == 9:
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ")
+        print(" ____________")
+    elif lives == 8:
+        print(" ")
+        print("|    ")
+        print("|  ")
+        print("|     ")
+        print("|    ")
+        print("|   ")
+        print("|")
+        print("|             ")
+        print("|____________")
+    elif lives == 7:
+        print("____________")
+        print("|      ")
+        print("|    ")
+        print("|      ")
+        print("|     ")
+        print("|       ")
+        print("|    ")
+        print("|             ")
+        print("|____________")
+    elif lives == 6:
+        print("____________")
+        print("|      |")
+        print("|    ")
+        print("|      ")
+        print("|      ")
+        print("|      ")
+        print("|     ")
+        print("|             ")
+        print("|____________")
+    elif lives == 5:
+        print("____________")
+        print("|      |")
+        print("|    ('-')")
+        print("|     ")
+        print("|     ")
+        print("|      ")
+        print("|     ")
+        print("|             ")
+        print("|____________")
+    elif lives == 4:
+        print("____________")
+        print("|      |")
+        print("|    ('-')")
+        print("|      |")
+        print("|      |   ")
+        print("|      ")
+        print("|     ")
+        print("|             ")
+        print("|____________")
+    elif lives == 3:
+        print("____________")
+        print("|      |")
+        print("|    ('-')")
+        print("|     /| ")
+        print("|    / |   ")
+        print("|        ")
+        print("|         ")
+        print("|             ")
+        print("|____________")
+    elif lives == 2:
+        print("____________")
+        print("|      |")
+        print("|    ('-')")
+        print("|     /|\ ")
+        print("|    / | \  ")
+        print("|        ")
+        print("|        ")
+        print("|             ")
+        print("|____________")
+    elif lives == 1:
+        print("____________")
+        print("|      |")
+        print("|    ('-')")
+        print("|     /|\ ")
+        print("|    / | \  ")
+        print("|     /   ")
+        print("|    /    ")
+        print("|             ")
+        print("|____________")
+    elif lives == 0:
+        print("____________")
+        print("|      |")
+        print("|    (X-X)")
+        print("|     /|\ ")
+        print("|    / | \  ")
+        print("|     / \  ")
+        print("|    /   \  ")
+        print("|             ")
+        print("|____________")
 #if the player has no lives left, display the end message, and ask if they want to play again.
-if lives == 0:
-    print ("_____end message____")
-    answer = input("Would you like to play again? Yes or No"). lower()
-    if answer == "Yes":
-        print("ok")
-    #loop back to the start(idk how rn)
-    elif answer == "no":
-        print ("Thank you for playing.")
+    if lives == 0:
+        print ("_____end message____")
+        answer = input("Would you like to play again? Yes or No\n"). lower()
+        if answer == "Yes":
+            print("")
+        #loop back to the start(idk how rn)
+        elif answer == "no":
+            print ("Thank you for playing.")
