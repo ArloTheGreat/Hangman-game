91905b5c1ee8fe27611d17ea153e35d1e9fcaaba
bnuaWSCW <159732805+bnuaWSCW@users.noreply.github.com>
2024-06-25 14:57:56 +1200

    Brandon Commit 6.0 25/06/2024

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 
 
 import random
-words = ("print", "words", "lucky", "space", "enter", "happy", "slope", "speed","brain","alone","cakes","dance","extra","farms","gamer","igloo","jeans","knock","laugh","messy","noble","ocean", "paint","queen", "radio", "salad", "table", "ultra", "voice", "wacky", "young", "zebra" ) 
+words = ("print", "words", "lucky", "space", "exits", "fetch", 
+         "slope", "speed","brain","alone","cakes","dance","extra","farms","gamer","froze",
+         "jeans","bangs","laugh","mudge","noble","ocean", "paint","kings", "radio", "crane", "table", "ultra", "voice", "wacky", "young", "zebra" ) 
 
 
 
 answer = "yes"
 
 guessed_words = []
+
+actual_word = []
 
 
 
@@ -28,14 +32,15 @@ while answer == "yes":
 
 # - Lives - #
     lives = 10
-
+    placehold_score = 0
 
 
 
     # -- Getting a random word from the 5 letter list
     differentword = random.choice(words)
+    actual_word.append(differentword)
 
-
+    print(actual_word)
 
 # --- GAME PROCESS --- #
     while lives > 0:
@@ -56,132 +61,127 @@ while answer == "yes":
                     print("Please guess 1 letter at a time\n")
                 elif length == 0 or guess == " ":
                     print("You haven't guessed a letter\n")
-
+                
                 else:
                     guessed_words.append(guess)
                     break
-        
-
-
-
-
+            
+        print(guessed_words)
+        if guess in differentword:
+            print("Correct! That is in the word")
+            placehold_score += 1
+        elif guess not in differentword:
+            lives -= 1
+            print("Incorrect, you got this! Try again\n")
+    
 #Hangman step depending on the
-    if lives == 10:
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-    elif lives == 9:
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ")
-        print(" ____________")
-    elif lives == 8:
-        print(" ")
-        print("|    ")
-        print("|  ")
-        print("|     ")
-        print("|    ")
-        print("|   ")
-        print("|")
-        print("|             ")
-        print("|____________")
-    elif lives == 7:
-        print("____________")
-        print("|      ")
-        print("|    ")
-        print("|      ")
-        print("|     ")
-        print("|       ")
-        print("|    ")
-        print("|             ")
-        print("|____________")
-    elif lives == 6:
-        print("____________")
-        print("|      |")
-        print("|    ")
-        print("|      ")
-        print("|      ")
-        print("|      ")
-        print("|     ")
-        print("|             ")
-        print("|____________")
-    elif lives == 5:
-        print("____________")
-        print("|      |")
-        print("|    ('-')")
-        print("|     ")
-        print("|     ")
-        print("|      ")
-        print("|     ")
-        print("|             ")
-        print("|____________")
-    elif lives == 4:
-        print("____________")
-        print("|      |")
-        print("|    ('-')")
-        print("|      |")
-        print("|      |   ")
-        print("|      ")
-        print("|     ")
-        print("|             ")
-        print("|____________")
-    elif lives == 3:
-        print("____________")
-        print("|      |")
-        print("|    ('-')")
-        print("|     /| ")
-        print("|    / |   ")
-        print("|        ")
-        print("|         ")
-        print("|             ")
-        print("|____________")
-    elif lives == 2:
-        print("____________")
-        print("|      |")
-        print("|    ('-')")
-        print("|     /|\ ")
-        print("|    / | \  ")
-        print("|        ")
-        print("|        ")
-        print("|             ")
-        print("|____________")
-    elif lives == 1:
-        print("____________")
-        print("|      |")
-        print("|    ('-')")
-        print("|     /|\ ")
-        print("|    / | \  ")
-        print("|     /   ")
-        print("|    /    ")
-        print("|             ")
-        print("|____________")
-    elif lives == 0:
-        print("____________")
-        print("|      |")
-        print("|    (X-X)")
-        print("|     /|\ ")
-        print("|    / | \  ")
-        print("|     / \  ")
-        print("|    /   \  ")
-        print("|             ")
-        print("|____________")
-#if the player has no lives left, display the end message, and ask if they want to play again.
-    if lives == 0:
-        print ("_____end message____")
-        answer = input("Would you like to play again? Yes or No\n"). lower()
-        if answer == "Yes":
-            print("")
-        #loop back to the start(idk how rn)
-        elif answer == "no":
-            print ("Thank you for playing.")
+        if lives == 9:
+            print(" ")
+            print(" ")
+            print(" ")
+            print(" ")
+            print(" ")
+            print(" ")
+            print(" ")
+            print(" ")
+            print(" ____________")
+        elif lives == 8:
+            print(" ")
+            print("|    ")
+            print("|  ")
+            print("|     ")
+            print("|    ")
+            print("|   ")
+            print("|")
+            print("|             ")
+            print("|____________")
+        elif lives == 7:
+            print("____________")
+            print("|      ")
+            print("|    ")
+            print("|      ")
+            print("|     ")
+            print("|       ")
+            print("|    ")
+            print("|             ")
+            print("|____________")
+        elif lives == 6:
+            print("____________")
+            print("|      |")
+            print("|    ")
+            print("|      ")
+            print("|      ")
+            print("|      ")
+            print("|     ")
+            print("|             ")
+            print("|____________")
+        elif lives == 5:
+            print("____________")
+            print("|      |")
+            print("|    ('-')")
+            print("|     ")
+            print("|     ")
+            print("|      ")
+            print("|     ")
+            print("|             ")
+            print("|____________")
+        elif lives == 4:
+            print("____________")
+            print("|      |")
+            print("|    ('-')")
+            print("|      |")
+            print("|      |   ")
+            print("|      ")
+            print("|     ")
+            print("|             ")
+            print("|____________")
+        elif lives == 3:
+            print("____________")
+            print("|      |")
+            print("|    ('-')")
+            print("|     /| ")
+            print("|    / |   ")
+            print("|        ")
+            print("|         ")
+            print("|             ")
+            print("|____________")
+        elif lives == 2:
+            print("____________")
+            print("|      |")
+            print("|    ('-')")
+            print("|     /|\ ")
+            print("|    / | \  ")
+            print("|        ")
+            print("|        ")
+            print("|             ")
+            print("|____________")
+        elif lives == 1:
+            print("____________")
+            print("|      |")
+            print("|    ('-')")
+            print("|     /|\ ")
+            print("|    / | \  ")
+            print("|     /   ")
+            print("|    /    ")
+            print("|             ")
+            print("|____________")
+        elif lives == 0:
+            print("____________")
+            print("|      |")
+            print("|    (X-X)")
+            print("|     /|\ ")
+            print("|    / | \  ")
+            print("|     / \  ")
+            print("|    /   \  ")
+            print("|             ")
+            print("|____________")
+    #if the player has no lives left, display the end message, and ask if they want to play again.
+        if lives == 0:
+            print ("_____end message____")
+            answer = input("Would you like to play again? Yes or No\n"). lower()
+            if answer == "Yes":
+                print("")
+            #loop back to the start(idk how rn)
+            elif answer == "no":
+                print ("Thank you for playing.")
+        
